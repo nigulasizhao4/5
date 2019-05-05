3660e024c2b6261d7e1cece41fba6ffc03aadd6c
xkwg <1219464774@qq.com>
2019-05-05 12:02:32 +0800

    first commit

---
+++ app/src/main/java/com/example/home1/myapplication/MyWebView.java
@@ -0,0 +1,51 @@
+package com.example.home1.myapplication;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+/**
+ * Created by qinghua on 2017/4/18.
+ */
+
+public class MyWebView extends AppCompatActivity {
+
+    private WebView webView;
+    private String url;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_web_view);
+        initView();
+        setWebView();
+    }
+
+    private void initView() {
+        webView= (WebView) findViewById(R.id.my_webView);
+        url=getIntent().getData().toString();
+    }
+
+
+    private void setWebView() {
+        WebSettings webSettings=webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setSupportZoom(true);
+
+        webView.loadUrl(url);
+        webView.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                webView.loadUrl(url);
+                return true;
+            }
+        });
+    }
+}
